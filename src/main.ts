05242fef94e2bf4f6193aa0d73496c8656fceda9
Alex-maslo <4439897@gmail.com>
2025-07-10 13:46:48 +0300

    fix: add global crypto to fix randomUUID ReferenceError in TypeORM

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import * as crypto from 'crypto';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
